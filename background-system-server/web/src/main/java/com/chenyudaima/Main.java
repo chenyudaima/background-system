ab98897e68105614757d342e616f085e5650e2dc
chenyudaima <1425791799@qq.com>
2022-12-20 13:42:20 +0800

    测试

---
+++ background-system-server/web/src/main/java/com/chenyudaima/Main.java
@@ -0,0 +1,11 @@
+package com.chenyudaima;
+
+/**
+ * @author 沉鱼代码
+ * @date ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
